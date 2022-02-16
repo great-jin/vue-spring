e04ae912d74202cc1fcf1d8568df24cd8e7ad7d3
yweijin <yweijin@linewell.com>
2022-02-16 16:34:25 +0800

    mail redis

---
+++ demo-manager/src/main/java/xyz/greatinvincible/utils/RedisSubscriber.java
@@ -0,0 +1,28 @@
+package xyz.greatinvincible.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Service
+public class RedisSubscriber {
+    private static String CHANNEL = "budai";
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    public RedisSubscriber(RedisTemplate redisTemplate) {
+        RedisConnection redisConnection = redisTemplate.getConnectionFactory().getConnection();
+        redisConnection.subscribe(new MessageListener() {
+            @Override
+            public void onMessage(Message message, byte[] bytes) {
+                // 收到消息的处理逻辑
+                log.info("Receive message : " + message);
+            }
+        }, CHANNEL.getBytes(StandardCharsets.UTF_8));
+    }
+}
